aa560d88b64fca3d52a8173fb99df056eb30e5a6
bless12ahadjie <blaise0ahadjie@gmail.com>
2024-01-10 11:33:40 +0000

    day 1

---
+++ week 1/Day 1/hello_world/main.rs
@@ -0,0 +1,20 @@
+// main function
+fn main(){
+
+    // variable declaration
+    let age: i32 = 26;
+    let name: &str ="Bless Ahadjie";
+
+    println! ("Hello {} you are {} years old",name ,age);
+
+    // function call
+    trial();
+}
+
+//  created function
+fn trial () {
+    let say_something = "Hey it's a new year and we are here to milk it";
+
+    println! ("{}",say_something);
+
+}
