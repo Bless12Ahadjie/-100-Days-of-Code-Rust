72d08e7f2b4ae1dcff04a2160f22451e950a6b0e
Bless Ahadjie <89154198+Bless12Ahadjie@users.noreply.github.com>
2024-01-12 10:19:19 +0000

    testing to see if i can change value of immutables

--- week 1/Day 3/common_concepts/src/main.rs
+++ week 1/Day 3/common_concepts/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let age = 20;
+
+    println!("You are now {age} years old");
+
+    age = 26;
+
+    println! ("You should be {age}  years old next 6 years");
 }
