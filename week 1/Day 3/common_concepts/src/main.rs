03b4ad54cd30bb28d5c30d16853e9d0504cdc615
Bless Ahadjie <89154198+Bless12Ahadjie@users.noreply.github.com>
2024-01-12 10:36:29 +0000

    shadowing and using of scope

--- week 1/Day 3/common_concepts/src/main.rs
+++ week 1/Day 3/common_concepts/src/main.rs
@@ -1,10 +1,21 @@
 fn main() {
-    let mut age = 20;
-    const NAME:&str = "Bless";
+    // Variables and Constants
+    // let mut age = 20;
+    // const NAME:&str = "Bless";
 
-    println!("{NAME} is now {age} years old");
+    // println!("{NAME} is now {age} years old");
 
-    age = 26;
+    // age = 26;
 
-    println! ("{NAME} should be {age}  years old next 6 years");
+    // println! ("{NAME} should be {age}  years old next 6 years");
+    let x = 5 ;
+    let x = x + 1;
+
+    {
+        let x = 5 * 2;
+
+        println! ("the inner scope is {x}")
+    }
+
+    println! ( "The outer scope becomes {x}")
 }
