ee458bc7d1d8c2e5dc69c4e53bc12ca1e3ddf414
Bless Ahadjie <89154198+Bless12Ahadjie@users.noreply.github.com>
2024-01-15 08:32:21 +0000

    Tested the initialized project

---
+++ week 1/Day 4/compound_datatypes/target/debug/compound_datatypes.d
@@ -0,0 +1,1 @@
+C:\Users\BlessAhadjie\OneDrive\ -\ AmaliTech\ gGmbH\Documents\GitHub\-100-Days-of-Code-Rust\week\ 1\Day\ 4\compound_datatypes\target\debug\compound_datatypes.exe: C:\Users\BlessAhadjie\OneDrive\ -\ AmaliTech\ gGmbH\Documents\GitHub\-100-Days-of-Code-Rust\week\ 1\Day\ 4\compound_datatypes\src\main.rs
---
+++ week 1/Day 4/compound_datatypes/target/debug/deps/compound_datatypes.d
@@ -0,0 +1,5 @@
+C:\Users\BlessAhadjie\OneDrive - AmaliTech gGmbH\Documents\GitHub\-100-Days-of-Code-Rust\week 1\Day 4\compound_datatypes\target\debug\deps\compound_datatypes.exe: src\main.rs
+
+C:\Users\BlessAhadjie\OneDrive - AmaliTech gGmbH\Documents\GitHub\-100-Days-of-Code-Rust\week 1\Day 4\compound_datatypes\target\debug\deps\compound_datatypes.d: src\main.rs
+
+src\main.rs:
