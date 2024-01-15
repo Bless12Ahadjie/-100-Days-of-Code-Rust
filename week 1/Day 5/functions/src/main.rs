e15a4bf9af5fd40b3c5548a07ee5c2775a7e2b0c
Bless Ahadjie <89154198+Bless12Ahadjie@users.noreply.github.com>
2024-01-15 17:44:50 +0000

    Functions with parameters

--- week 1/Day 5/functions/src/main.rs
+++ week 1/Day 5/functions/src/main.rs
@@ -5,6 +5,14 @@ fn my_func (){
     println!("Hey {name} this is your own defined function. HURRAY!!!");
 }
 
+// function with parameter
+fn add (num1:u32 , num2: u32){
+  let sum = num1 + num2;
+  println! {"The addition is {sum}"};
+}
+
+//main function
 fn main() {
-   my_func();
+   add(4,16);
+//    my_func();
 }
