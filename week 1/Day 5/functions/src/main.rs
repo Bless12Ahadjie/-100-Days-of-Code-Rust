216601f9b2c38e5edfa2499c5348775fcfb9d94a
Bless Ahadjie <89154198+Bless12Ahadjie@users.noreply.github.com>
2024-01-15 17:38:55 +0000

    interchanging positions of functions

--- week 1/Day 5/functions/src/main.rs
+++ week 1/Day 5/functions/src/main.rs
@@ -1,9 +1,10 @@
-fn main() {
-   my_func();
-}
 
 fn my_func (){
     let name: &str = "Bless";
 
     println!("Hey {name} this is your own defined function. HURRAY!!!");
+}
+
+fn main() {
+   my_func();
 }
