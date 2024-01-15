fbbf4bef56d250a9f380f0d192d6f06441f39a4b
Bless Ahadjie <89154198+Bless12Ahadjie@users.noreply.github.com>
2024-01-15 08:43:41 +0000

    prefixing unused variables with "_"

--- week 1/Day 4/compound_datatypes/src/main.rs
+++ week 1/Day 4/compound_datatypes/src/main.rs
@@ -3,4 +3,14 @@ fn main() {
     let tup = (500,4.5,1);
     let (a,_b,_c) = tup;
     println!("{a}");
+
+    let x: (i32, f64, u8) = (500, 6.4, 1);
+
+    let _five_hundred = x.0;
+
+    let _six_point_four = x.1;
+
+    let one = x.2;
+
+    println!("{one}");
 }
