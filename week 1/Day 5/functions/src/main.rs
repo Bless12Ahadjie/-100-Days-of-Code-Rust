f68c71bd916d222f4bf5fce65c3d2389accfdd9d
Bless Ahadjie <89154198+Bless12Ahadjie@users.noreply.github.com>
2024-01-16 07:40:35 +0000

    fixes

--- week 1/Day 5/functions/src/main.rs
+++ week 1/Day 5/functions/src/main.rs
@@ -1,30 +1,29 @@
-
-fn my_func (){
+fn my_func() {
     let name: &str = "Bless";
 
     println!("Hey {name} this is your own defined function. HURRAY!!!");
 }
 
 // function with parameter
-fn add (num1:u32 , num2: u32){
-  let sum: u32 = num1 + num2;
-  println! {"The addition is {sum}"};
+fn add(num1: u32, num2: u32) {
+    let sum: u32 = num1 + num2;
+    println! {"The addition is {sum}"};
 }
 
 //function with a return value
-fn retu (a:i32 , b:i32) ->  i32 {
-   let sum: i32 =  a + b;
-   sum
-} 
+fn retu(a: i32, b: i32) -> i32 {
+    let sum: i32 = a + b;
+    sum
+}
 
 //main function
 fn main() {
- println!("This return function returns {}",retu(1,4));
-//   println!("This return function returns {hold}");
+    println!("This return function returns {}", retu(1, 4));
+    //   println!("This return function returns {hold}");
 
-   add(4,16);
+    add(4, 16);
 
-   my_func();
+    my_func();
 }
 
-//build a basic calcutator function 
+//build a basic calcutator function
