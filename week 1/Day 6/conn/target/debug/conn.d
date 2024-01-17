4634b9c9fa33a9b62f135c4f8f92037a74a4d19f
Bless Ahadjie <89154198+Bless12Ahadjie@users.noreply.github.com>
2024-01-17 11:18:21 +0000

    running  "hello world" for day 6

---
+++ week 1/Day 6/conn/target/debug/conn.d
@@ -0,0 +1,1 @@
+C:\Users\BlessAhadjie\OneDrive\ -\ AmaliTech\ gGmbH\Documents\GitHub\-100-Days-of-Code-Rust\week\ 1\Day\ 6\conn\target\debug\conn.exe: C:\Users\BlessAhadjie\OneDrive\ -\ AmaliTech\ gGmbH\Documents\GitHub\-100-Days-of-Code-Rust\week\ 1\Day\ 6\conn\src\main.rs
---
+++ week 1/Day 6/conn/target/debug/deps/conn.d
@@ -0,0 +1,5 @@
+C:\Users\BlessAhadjie\OneDrive - AmaliTech gGmbH\Documents\GitHub\-100-Days-of-Code-Rust\week 1\Day 6\conn\target\debug\deps\conn.exe: src\main.rs
+
+C:\Users\BlessAhadjie\OneDrive - AmaliTech gGmbH\Documents\GitHub\-100-Days-of-Code-Rust\week 1\Day 6\conn\target\debug\deps\conn.d: src\main.rs
+
+src\main.rs:
