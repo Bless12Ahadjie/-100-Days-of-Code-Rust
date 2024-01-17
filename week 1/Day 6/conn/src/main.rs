20a684cce95998bd51e38457808c6358a991736d
Bless Ahadjie <89154198+Bless12Ahadjie@users.noreply.github.com>
2024-01-17 11:40:49 +0000

    Implementing a basic voting system

--- week 1/Day 6/conn/src/main.rs
+++ week 1/Day 6/conn/src/main.rs
@@ -1,13 +1,14 @@
 fn main() {
-    let number = 6;
+    let age: u32 = 20;
+    let eligible:u16 = 18;
 
-    if number % 4 == 0 {
-        println!("number is divisible by 4");
-    } else if number % 3 == 0 {
-        println!("number is divisible by 3");
-    } else if number % 2 == 0 {
-        println!("number is divisible by 2");
-    } else {
-        println!("number is not divisible by 4, 3, or 2");
+    if age => eligible{
+        println!("You are eligible to vote");
+    }
+    else if age < eligible {
+        println!(" You are not eligible to vote");
+    }
+    else {
+        println!("You are an align");
     }
 }
