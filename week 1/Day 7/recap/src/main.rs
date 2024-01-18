4c84309ad97fc4b3a68f1a8fde8efbbdaa8b8ba3
Bless Ahadjie <89154198+Bless12Ahadjie@users.noreply.github.com>
2024-01-18 13:28:56 +0000

    taking input works

--- week 1/Day 7/recap/src/main.rs
+++ week 1/Day 7/recap/src/main.rs
@@ -1,24 +1,23 @@
+
 use std::io;
+
 fn main() {
-    let mut age = String::new();  
+    let mut age = String::new();
 
     println!("How old are you mate? ");
 
     io::stdin()
         .read_line(&mut age)
-        .expect("Failed to read lone");
+        .expect("Failed to read line");
 
     let age: u32 = match age.trim().parse() {
         Ok(num) => num,
-        Err(_)  => println!("Error"), 
-    }
-    
-    println!("You are {age} years old");
-    
-    // {
-    //     Ok(_) => {
-    //          println!("Hey you are {} years old", age);
-    //      }
-    //     Err(e) => println!("I didnt read anything : {}", e);
-    // }   
+        Err(_) => {
+            println!("Error");
+            0 // or any default value you want to assign in case of an error
+        }
+    };
+
+    println!("You are {} years old", age);
 }
+
