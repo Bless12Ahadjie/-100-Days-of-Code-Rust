ec33d5aaefaf72bd54783c705dd601846d973a33
Bless Ahadjie <89154198+Bless12Ahadjie@users.noreply.github.com>
2024-01-18 15:55:12 +0000

    few changes for trials from memory

--- week 1/Day 7/recap/src/main.rs
+++ week 1/Day 7/recap/src/main.rs
@@ -1,41 +1,8 @@
+use std::io; 
 
-use std::io;
-
-fn main() {
+fn main (){
     let mut age = String::new();
 
-    loop{
-        
-    println!("How old are you mate? ");
-
-    io::stdin()
-        .read_line(&mut age)
-        .expect("Failed to read line");
-
-    let age: u32 = match age.trim().parse() {
-        Ok(num) => num,
-        Err(e) => {
-            println!("Error {}",e);
-            0 
-        }
-    };
-
-    if age >= 18 {
-        println!("You are eligible to vote");
-    }
-    else if age < 18 {
-        println!("You are not eligible to vote");
-    }
-    else {
-        println!("Hey mate! I think you are an alain");
-  
-    }
-
-   
-    }
-    
-
-
+    println!("Enter age ");
     
 }
-
