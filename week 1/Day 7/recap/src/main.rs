25899d0f1205dafae5264c3860c58e5d9a66961c
Bless Ahadjie <89154198+Bless12Ahadjie@users.noreply.github.com>
2024-01-18 15:58:49 +0000

    fixed from memory

--- week 1/Day 7/recap/src/main.rs
+++ week 1/Day 7/recap/src/main.rs
@@ -4,5 +4,18 @@ fn main (){
     let mut age = String::new();
 
     println!("Enter age ");
-    
+
+    io::stdin()
+        .read_line(&mut age)
+        .expect("Failed");
+
+    let age: u32 = match age.trim().parse(){
+        Ok(a)=> a,
+        Err(e) =>{
+            println!("Error: {}", e);
+            0
+        }
+    };
+
+    println!{"You are {} years old", age};
 }
