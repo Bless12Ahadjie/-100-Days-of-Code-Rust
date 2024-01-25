5dc94ad390c6909cb3cef3801b26e17a96aa1f3a
Bless Ahadjie <89154198+Bless12Ahadjie@users.noreply.github.com>
2024-01-25 09:27:22 +0000

    re-writing codes for week 2 day 2

--- week 2/Day 2/day2/src/main.rs
+++ week 2/Day 2/day2/src/main.rs
@@ -1,14 +1,4 @@
-use std::io;
+fn main () {
 
-fn main (){
-   
-}
-fn ownership (){
- println!("Enter a num", );
- let mut guess = String::new ();
-
- io::stdin()
-   .read_line(%);
-
-
+  println!("Hello, world!");
 }
