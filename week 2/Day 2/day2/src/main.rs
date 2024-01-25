b86d8c1dba955652c83d981fe9cbbe8a20154d45
Bless Ahadjie <89154198+Bless12Ahadjie@users.noreply.github.com>
2024-01-25 09:08:39 +0000

    Update main.rs

--- week 2/Day 2/day2/src/main.rs
+++ week 2/Day 2/day2/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+
+fn main (){
+   
+}
+fn ownership (){
+ println!("Enter a num", );
+ let mut guess = String::new ();
+
+ io::stdin()
+   .read_line(%);
+
+
 }
